7d1a10723e872e53180ffb63ab187e53eb50c986
Thomas Capellazzi <thomascapellazzi@MacBook-Pro-di-Thomas.local>
2019-02-05 18:25:20 +0100

    minigallery

--- public/parts/minigallery.php
+++ public/parts/minigallery.php
@@ -1,11 +1,10 @@
 <?php
 $minigallery = $this->getModulo('Minigallery');
-
 if(count($minigallery)>0){
   echo "<ul class=\"minigallery clearfix\">";
     for($i=0; $i<count($minigallery); $i++){
       echo "<li>
-      <a class=\"swipebox\" href=\"".$this->getImg($minigallery[$i]['files'])."\" rel=\"gallery\" title=\"".$minigallery['immagini'][$i]['titolo']."\">
+      <a class=\"photobox\" href=\"".$this->getImg($minigallery[$i]['files'])."\" rel=\"gallery\" title=\"".$minigallery['immagini'][$i]['titolo']."\">
       <picture>
       <source media=\"(max-width: 750px)\" srcset=\"".$this->getImg($minigallery[$i]['files'])."\">
       <source media=\"(max-width: 1024px)\" srcset=\"".$this->getImg($minigallery[$i]['files'])."\">
